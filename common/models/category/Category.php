9c81f80eb580e7213cbef07cdf1da71fca7aad7c
Vitaliy Ofat <ofatv22@gmail.com>
2014-05-03 21:42:07 +0700

    added category lang support & fixed errors

---
+++ common/models/category/Category.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace yashop\common\models\category;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+use yashop\common\models\item\Item;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property integer $parent_id
+ * @property string $url
+ *
+ * @property Item[] $items
+ */
+class Category extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['parent_id', 'url'], 'required'],
+            [['parent_id'], 'integer'],
+            [['url'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('payment', 'ID'),
+            'parent_id' => Yii::t('payment', 'Parent ID'),
+            'url' => Yii::t('payment', 'Url'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItems()
+    {
+        return $this->hasMany(Item::className(), ['category_id' => 'id']);
+    }
+}
